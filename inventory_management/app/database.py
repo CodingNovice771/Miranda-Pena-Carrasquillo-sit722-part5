747fd41e0974cf3f1e8fd7eb0fa8592817e60533
Miranda Pena Carrasquillo <s223235859@deakin.edu.au>
2024-09-12 14:34:17 +1000

    Pushing to Repo

---
+++ inventory_management/app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+##SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+SQLALCHEMY_DATABASE_URL = "postgresql://miranda_pena_carrasquillo:5K0QsSQvdlIUFeizNDewOvul7jdPGs6z@dpg-crfskcbv2p9s73a2b9lg-a.singapore-postgres.render.com/library_db_izwe"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
